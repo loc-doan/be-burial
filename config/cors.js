b331a9a5cbad3bfd8ed105ccb2d1784141829176
locdoan <doanphucloc23@gmail.com>
2025-09-21 10:46:54 +0700

    fix3

--- config/cors.js
+++ config/cors.js
@@ -1,10 +1,16 @@
 
 const cors = require('cors');
 
+const allowedOrigins = ['http://localhost:3000', 'https://beacon-burial-drab.vercel.app/'];
+
 const corsOptions = {
-  origin: [
-    'http://localhost:3000'
-  ],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
